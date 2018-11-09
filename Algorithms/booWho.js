35b9f66a500a53b0e89a5ce2c7352acdf54ba704
Trent Teebken <trenttee1020@gmail.com>
2018-11-08 22:22:09 -0800

    Update booWho.js

--- Algorithms/booWho.js
+++ Algorithms/booWho.js
@@ -1,8 +1,6 @@
 /*
-
 Check if a value is classified as a boolean primitive. Return true or false.
 Boolean primitives are true and false.
-
 */
 const booWho = bool => {
   return typeof bool == 'boolean'
