bc114b5f1c2d988c56053619d3a9c58917aa0e2b
Trent Teebken <trenttee1020@gmail.com>
2018-11-13 10:26:01 -0800

    Update SliceAndSplice.js

--- Algorithms/SliceAndSplice.js
+++ Algorithms/SliceAndSplice.js
@@ -5,12 +5,9 @@ Return the resulting array. The input arrays should remain the same after the fu
 */
 function frankenSplice(arr1, arr2, n) { 
 	let copy = arr2.slice();
-
 	for (let i = 0; i < arr1.length; i++) {
-		copy.splice(n, 0, arr1[i]);
-		n++;
+		copy.splice(n++, 0, arr1[i]);
 	}
-
-	return localArray;
+	return copy;
 }
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1)); // [4, 1, 2, 3, 5, 6]
